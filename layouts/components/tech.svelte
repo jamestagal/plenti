c337f7c7a5426a3bac0e073096180c0b37ae8c31
jamestagal <benjaminjameswaller@gmail.com>
2022-10-04 00:10:35 +1100

    add tech stack section

---
+++ layouts/components/tech.svelte
@@ -0,0 +1,12 @@
+<section class="section-tech blog" id="blog" aria-label="blog">
+    <div class="container">
+
+      <p class="section-subtitle text-center has-before"> TECH</p>
+
+      <div class="logos-list"><img src="./assets/Jamstack_Logo_Original_Transparent.png" alt="Jamstack logo" width="250" height="200">
+      </div>
+      <div class="logos-list"><img src="./assets/svelte-logo.svg" alt="Svelte logo" width="50" height="50"><img src="./assets/Go-Logo_Blue.svg" alt="Go logo" width="125" height="100"> <img src="./assets/github.svg" alt="Github logo" width="50" height="50">
+    </div>
+
+    </div>
+  </section>
